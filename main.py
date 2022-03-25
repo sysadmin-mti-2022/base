faba690ca5e6c8cd13a4ac065fc5a3ea77f4e17d
Maximilinano Osorio <mosorio@isi.edu>
2022-03-25 15:35:16 -0300

    add: start point

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"Hola": "Mundo"}
 
 # @app.get("/items/{item_id}")
 # def read_item(item_id: int, q: Optional[str] = None):
